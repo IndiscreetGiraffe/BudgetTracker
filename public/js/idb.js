3e93c29fc40e0698eb01c2e9e66e91afa047120d
Kyleah Justice <kikij432@gmail.com>
2022-02-28 18:47:16 -0700

    this is nice, I like typing a lot, here's more of the code to implement idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,15 +1,13 @@
 let db;
-
 const request = indexedDB.open('BudgetTracker', 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore('new_transaction', { autoIncrement: true });
- };
+};
 
 request.onsuccess = function(event) {
     db=event.target.result;
-
     if(navigator.onLine) {
         uploadTransaction();
     }
@@ -17,4 +15,33 @@ request.onsuccess = function(event) {
 
 request.onerror = function(event) {
     console.log(event.target.errorCode);
- }; 
+}; 
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const trackerObjectStore = transaction.objectStore('new_transaction');
+    trackerObjectStore.add(record);
+};
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const trackerObjectStore = transaction.objectStore('new_transaction');
+    const getAll = trackerObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'applicattion/json'
+                }
+            })
+        }
+    }
+}
+
+
+  
+
