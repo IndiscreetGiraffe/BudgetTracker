8b7eff9f2ffd43889129b10539695d5178d3d9a7
Kyleah Justice <kikij432@gmail.com>
2022-02-28 18:35:04 -0700

    following tutorial on implementing the indexedDB, its fairly easy to follow and understand

---
+++ public/js/idb.js
@@ -0,0 +1,8 @@
+let db;
+
+const request = indexedDB.open('BudgetTracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
